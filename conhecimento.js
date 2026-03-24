8c09a82b201901cb1f0288396698433556e51570
Klaywer2019 <keisejemima@gmail.com>
2026-03-23 21:57:36 -0300

    Criar conhecimento.js

---
+++ conhecimento.js
@@ -0,0 +1,9 @@
+const dicionarioExpandido = {
+    trap_br: ["vivência", "progresso", "fuga", "notas"],
+    trap_en: ["lifestyle", "hype", "cash", "gang"],
+    conexoes: {
+        "progresso": "success",
+        "fuga": "skrt",
+        "notas": "bills"
+    }
+}
